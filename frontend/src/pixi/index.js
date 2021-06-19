4a9b9d0645170edc5894e559db1e71bf92d170bd
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-06-19 14:13:27 +0530

    update: map renderer

--- frontend/src/pixi/index.js
+++ frontend/src/pixi/index.js
@@ -1,15 +1,14 @@
 import * as PIXI from "pixi.js";
 import Player from "./components/PlayerClass";
-
-import gameState from "./game-state.json";
-
+import MapManager from "./MapManager";
+import layers from "./layers";
 import { init as pointerInit } from "./controllers/pointer";
 import { init as keyboardInit } from "./controllers/keyboard";
 
 const PixiApp = new PIXI.Application({
-	width: window.innerWidth * 0.75,
-	height: window.innerHeight,
-	backgroundColor: 0x1099bb,
+	width: 48 * 22,
+	height: 48 * 18,
+	backgroundColor: 0xffffff,
 });
 
 pointerInit();
@@ -18,10 +17,29 @@ keyboardInit();
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 const world = new PIXI.Container();
-PixiApp.stage.addChild(world);
 
-const player1 = new Player("player1", 0xff0000, 50, 50);
-world.addChild(player1);
+world.width = 48 * 22;
+world.height = 48 * 18;
+
+const setup = () => {
+	PixiApp.stage.addChild(world);
+
+	console.log(PIXI.Loader.shared.resources);
+	let sheet = PIXI.Loader.shared.resources["assets/spritesheet.json"].spritesheet;
+
+	const sprites = new MapManager(22, 18, 48, sheet, layers).sprites;
+
+	sprites.forEach((sprite) => {
+		world.addChild(sprite);
+	});
+
+	const player1 = new Player("player1", 0xff0000, 50, 50);
+	world.addChild(player1);
+
+	console.log(world);
+};
+
+PIXI.Loader.shared.add("assets/spritesheet.json").load(setup);
 
 const render = () => {
 	requestAnimationFrame(render);
