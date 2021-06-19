f89eac838c893d787192c0eab46aea8dc52dd78f
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-06-19 14:30:09 +0530

    fix: canvas pointer offset

--- frontend/src/pixi/controllers/pointer.js
+++ frontend/src/pixi/controllers/pointer.js
@@ -2,8 +2,15 @@ import gameState from "../game-state.json";
 
 const init = () => {
 	document.body.onpointermove = (event) => {
-		gameState.controls.pointerX = event.pageX;
-		gameState.controls.pointerY = event.pageY;
+		let canvas = document.getElementsByTagName("canvas")[0];
+		let offsetLeft = 0;
+		let offsetTop = 0;
+		if (canvas) {
+			offsetLeft = canvas.offsetLeft;
+			offsetTop = canvas.offsetTop;
+		}
+		gameState.controls.pointerX = event.pageX - offsetLeft;
+		gameState.controls.pointerY = event.pageY - offsetTop;
 	};
 };
 
