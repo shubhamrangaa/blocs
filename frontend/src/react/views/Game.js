9ff5ceda6a64532092c811e9d8458033f41d6523
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-06-20 10:13:12 +0530

    style: game page layout

--- frontend/src/react/views/Game.js
+++ frontend/src/react/views/Game.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import StartButton from "../components/StartButton";
 import styles from "../styles/Game.module.css";
+
+import { layout, controller } from "../styles/Layout.module.css";
 export const Game = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div id="container">
-      <div id="pixi-root"></div>
-      <section className={styles.signupContainer}>
-        <h1>Game Page</h1>
+    <div className={layout}>
+      <div id="container">
+        <div id="pixi-root"></div>
+      </div>
+      <div className={controller}>
+        {/* <h1 style={{ hidden: `${show}` }}>Game Page</h1> */}
         <StartButton />
-      </section>
+      </div>
     </div>
   );
 };
