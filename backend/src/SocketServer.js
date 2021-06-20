d2aa99f72c90d752d721da9c891f8336907b63ff
Divyanshu Sharma <32746859+d1vshar@users.noreply.github.com>
2021-06-20 15:47:13 +0530

    Update SocketServer.js

--- backend/src/SocketServer.js
+++ backend/src/SocketServer.js
@@ -8,7 +8,7 @@ export default class SocketServer {
     this.httpServer = createServer();
     this.io = new Server(this.httpServer, {
       cors: {
-        origin: 'https://blocs-frontend.vercel.app/',
+        origin: 'https://blocs-frontend.vercel.app',
         methods: ['GET', 'POST'],
       },
     });
