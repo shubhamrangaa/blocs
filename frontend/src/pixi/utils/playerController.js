751142d88a4c50707f31d16b5620d844c1fea8b7
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-06-20 07:36:57 +0530

    add: player position tracker

---
+++ frontend/src/pixi/utils/playerController.js
@@ -0,0 +1,23 @@
+import * as PIXI from "pixi.js";
+import Bump from "bump.js";
+
+import * as gameState from "../game-state.json";
+
+const b = new Bump(PIXI);
+
+const collideProps = (player, props) => {
+	for (let i = 0; i < props.length; i++) {
+		if (props[i].isRound) b.circleCollision(player, props[i]);
+		else b.circleRectangleCollision(player, props[i]);
+	}
+};
+
+const updateCharacterState = (player) => {
+	// gameState.x = player.getBounds().x;
+	// gameState.y = player.getBounds().y;
+	gameState.x = player.position.x;
+	gameState.y = player.position.y;
+	console.log(gameState);
+};
+
+export { collideProps, updateCharacterState };
